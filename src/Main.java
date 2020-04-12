a7de9835b5353d1e2816c87ecc4c74583cd5b02f
Ficus <Ficus@local.com>
2020-04-12 21:16:08 +0800

    Pro: Tank War - Initialization A frame for the game

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Frame f = new Frame("tank war");
+        f.setLocation(400,100);
+        f.setSize(800,600);
+        f.setVisible(true);
+    }
+}
