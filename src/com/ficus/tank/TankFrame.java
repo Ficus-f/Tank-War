071107dd08e0046237088032bbd23fe1f0cd7b48
Ficus <Ficus@local.com>
2020-04-14 22:01:51 +0800

    Double buffering to resolve screen flicker. Override update() method

--- src/com/ficus/tank/TankFrame.java
+++ src/com/ficus/tank/TankFrame.java
@@ -8,10 +8,13 @@ public class TankFrame extends Frame {
     private Tank myTank;
     private Tank enemy;
 
+    private static final int GAME_WIDTH = 800;
+    private static final int GAME_HEIGHT = 600;
+
     public TankFrame() {
         this.setTitle("tank war");
         this.setLocation(400, 100);
-        this.setSize(800, 600);
+        this.setSize(GAME_WIDTH, GAME_HEIGHT);
 
         this.addKeyListener(new TankKeyListener());
 
@@ -23,6 +26,22 @@ public class TankFrame extends Frame {
     public void paint(Graphics g) {
         myTank.paint(g);
         enemy.paint(g);
+    }
+
+    Image offScreenImage = null;
+
+    @Override
+    public void update(Graphics g) {
+        if (offScreenImage == null) {
+            offScreenImage = this.createImage(GAME_WIDTH, GAME_HEIGHT);
+        }
+        Graphics gOffScreen = offScreenImage.getGraphics();
+        Color c = gOffScreen.getColor();
+        gOffScreen.setColor(Color.BLACK);
+        gOffScreen.fillRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
+        gOffScreen.setColor(c);
+        paint(gOffScreen);
+        g.drawImage(offScreenImage, 0, 0, null);
     }
 
     private class TankKeyListener extends KeyAdapter {
